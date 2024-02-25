99d5679374b27f4de728787b60769a192dc29427
kavin k <kavin k@DESKTOP-EBLQ19S>
2024-02-25 12:04:38 +0530

    push1

---
+++ src/testScript/Test1.java
@@ -0,0 +1,17 @@
+package testScript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Test1 
+{
+	@Test
+	public void test()
+	{
+		System.setProperty("webdriver.gecko.driver","./geckodriver.exe");
+		WebDriver driver=new FirefoxDriver();
+		driver.get("https://www.facebook.com");
+		driver.quit();
+	}
+}
